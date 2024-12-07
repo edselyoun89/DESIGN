876fc047c5522b7435febffe8eafaea1abbb5a05
edselyoun89 <nikita.telichko.gmail.com@yandex.ru>
2024-12-07 16:38:24 +0300

    Пункт 7.1: Добавлен поиск по дизайнам на главной странице

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const Design = require('../models/Design'); // Подключение модели Design
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   try {
     const designs = await Design.find(); // Получить все дизайны из базы данных
     res.render('index', { 
@@ -34,21 +34,31 @@ router.post('/add', async function (req, res, next) {
   }
 });
 
-/* GET страницы дизайнов по названию (title). */
-router.get('/:designTitle', async function(req, res, next) {
+/* GET запрос для поиска дизайнов */
+router.get('/search', async function (req, res, next) {
   try {
-    const design = await Design.findOne({ title: req.params.designTitle }); // Найти дизайн по названию
-    if (!design) {
-      return res.status(404).send('Дизайн не найден');
+    const query = req.query.q; // Получить строку поиска из параметров запроса
+    const designs = await Design.find({
+      title: { $regex: query, $options: 'i' } // Регистронезависимый поиск по названию
+    });
+
+    // Если результаты поиска пусты
+    if (designs.length === 0) {
+      return res.render('index', { 
+        title: 'Результаты поиска', 
+        designs: [], 
+        message: 'Дизайны не найдены' 
+      });
     }
-    res.render('design', {
-      title: design.title,
-      picture: design.picture,
-      desc: design.desc
+
+    // Отобразить найденные дизайны
+    res.render('index', { 
+      title: 'Результаты поиска', 
+      designs 
     });
   } catch (err) {
-    console.error('Ошибка загрузки дизайна:', err.message);
-    res.status(500).send('Ошибка загрузки данных');
+    console.error('Ошибка поиска дизайнов:', err.message);
+    res.status(500).send('Ошибка поиска');
   }
 });
 
@@ -78,7 +88,6 @@ router.post('/edit/:id', async function (req, res, next) {
   }
 });
 
-
 /* GET запрос для удаления дизайна */
 router.get('/delete/:id', async function (req, res, next) {
   try {
@@ -90,4 +99,23 @@ router.get('/delete/:id', async function (req, res, next) {
   }
 });
 
+/* GET страницы дизайнов по названию (title). */
+router.get('/:designTitle', async function (req, res, next) {
+  try {
+    const design = await Design.findOne({ title: req.params.designTitle }); // Найти дизайн по названию
+    if (!design) {
+      return res.status(404).send('Дизайн не найден');
+    }
+    res.render('design', {
+      title: design.title,
+      picture: design.picture,
+      desc: design.desc
+    });
+  } catch (err) {
+    console.error('Ошибка загрузки дизайна:', err.message);
+    res.status(500).send('Ошибка загрузки данных');
+  }
+});
+
 module.exports = router;
+
