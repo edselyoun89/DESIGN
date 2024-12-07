d3bf5d67cfdc17f82f671fda792259e35e627c10
edselyoun89 <nikita.telichko.gmail.com@yandex.ru>
2024-12-07 14:23:26 +0300

    Выполнение пункта 4.4: подключение ejs-locals

--- app.js
+++ app.js
@@ -39,3 +39,34 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var engine = require('ejs-locals'); // Подключение ejs-locals
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// Установка шаблонизатора ejs-locals
+app.engine('ejs', engine);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Подключение middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Маршруты
+app.use('/', index);
+app.use('/users', users);
+
+module.exports = app;
+
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Добро пожаловать на Design Project' });
+});
+
+
 /* GET страницы дизайнов. */
 router.get('/MinDesign', function(req, res, next) {
   res.render('design', {
