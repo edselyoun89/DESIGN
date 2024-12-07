f38d61f7a001ddc919cd7f33a67c0f32ed6c3215
edselyoun89 <nikita.telichko.gmail.com@yandex.ru>
2024-12-07 14:12:58 +0300

    Выполнение пункта 4.3: страницы дизайнов

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET страницы дизайнов. */
 router.get('/MinDesign', function(req, res, next) {
-  res.send("<h1>Страница Минималистичного Дизайна</h1>")
+  res.render('design', {
+    title: "Минималистичный Дизайн",
+    picture: "/images/minDesign.jpg",
+    desc: "Минималистичный дизайн характеризуется простотой, чистотой и функциональностью."
+  });
 });
 
 router.get('/ModDesign', function(req, res, next) {
-  res.send("<h1>Страница Модерн Дизайна</h1>")
+  res.render('design', {
+    title: "Модерн Дизайн",
+    picture: "/images/modernDesign.jpg",
+    desc: "Модерн дизайн подчеркивает современные элементы, яркие цвета и инновационные формы."
+  });
 });
 
-router.get('/MinDesign', function(req, res, next) {
-  res.send("<h1>Страница Ретро Дизайна</h1>")
+router.get('/RetroDesign', function(req, res, next) {
+  res.render('design', {
+    title: "Ретро Дизайн",
+    picture: "/images/retroDesign.jpg",
+    desc: "Ретро дизайн сочетает в себе элементы из прошлого и уникальную атмосферу ностальгии."
+  });
 });
 
 module.exports = router;
